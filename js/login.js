fc17193c2b1624827a769ff414a30e03c8e3077f
David-Wang163 <davidwang.oct2@gmail.com>
2020-05-01 12:22:22 -0700

    Added js directory with login.js & loginAuth.js.

---
+++ js/login.js
@@ -0,0 +1,9 @@
+$("a[href$='#modal-signup']").on("click",function () {
+    $("modal-signup").modal("show");
+});
+$("a[href$='#modal-login']").on("click",function () {
+    $("modal-login").modal("show");
+});
+$("a[href$='#modal-book']").on("click",function () {
+    $("modal-account").modal("show");
+});
