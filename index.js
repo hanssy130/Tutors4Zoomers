0fe62a8f1bb63ad0f6aa9e4e9485339c1c62f09c
Hans Sy <55111820+hanssy130@users.noreply.github.com>
2020-05-07 00:54:43 -0700

    Added index.js and Procfile. Testing on Heroku

--- index.js
+++ index.js
@@ -1,104 +1,23 @@
+// Uses Heroku's port or local port 5000.
+var PORT = process.env.PORT || 5000;
+
 const express = require("express");
-const bodyParser = require("body-parser");
-const admin = require("firebase-admin");
-const serviceAccount = require("./test-for-firestore-firebase-adminsdk-fvvlh-e6a9cc9aed.json")
 
-// Initialize Express
-// =========================================
+let app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
+// app.get("/", (req, res) => res.render("pages/index"));
 
-const app = express();
+app.get("/myForm", (req, res) => res.render("pages/myForm"));
 
-
-// Initialize FireStore
-// ========================================
-
-admin.initializeApp({credential: admin.credential.cert(serviceAccount)});
-const db = admin.firestore();
-
-
-// Initalize view engine and body parser
-// =========================================
-
-app.set ("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(__dirname + "/public"));
-
-
-// Landing Page
-// ======================================
-
-
-app.get("/", (req, res) => {
-    res.render("home");
+app.get("/", function (req, res) {
+  res.send("send this text");
 });
 
-
-// Login Page
-// ======================================
-
-
-app.get("/login", (req, res) => {
-    res.render("login");
+app.get("/cstisgreat", function (req, res) {
+  res.send("CST is great");
 });
 
-// Sign Up
-// =====================================
-
-app.get("/signup", (req, res) => {
-    res.render("signup");
-});
-
-
-// feature page
-// =======================================
-
-app.get("/feature", (req, res) => {
-    db.collection('test').doc("george").get().then( doc => {
-        if (!doc.exists){
-            console.log("No doc")
-            res.render("feature", {
-            });
-        }
-        else{
-            userData = doc.data()
-            res.render("feature", {
-                info: userData
-            });
-        }
-    })
-    .catch(err => {
-        console.log(err)
-    })
-
-});
-
-
-app.post("/feature", (req, res) => {
-    title = req.body.title
-    data = {
-        title: req.body.title,
-        body: req.body.info
-    };
-    db.collection("test").doc(title).set(data, {merge: true});
-    res.redirect('feature');
-});
-
-
-
-
-
-
-
-
-
-
-
-// port setup
-// ==========================================
-
-let port = 3001
-
-app.listen(port, ()=> {
-    console.log("Server has started ")
-})
+app.listen(PORT);
