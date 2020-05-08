69d1124a721460bc6a3a6210373644f9fb567414
David-Wang163 <davidwang.oct2@gmail.com>
2020-05-07 19:21:08 -0700

    Created app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+let app = express();
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.static("public"));
+    app.set("view engine", "ejs");
+const port = 3000;
+
+let roomName = [];
+
+app.get("/", (req, res) => res.render("pages/chatroom.ejs"));
+app.get("/chatroomForm", (req, res) => {
+    res.render("pages/chatroomForm");
+});
+
+app.post("/chatroomForm", (req, res) => {
+    let formData = req.body;
+    console.log(formData);
+    roomName.push(formData["roomName"]);
+    console.log(roomName)
+    res.render("pages/chatroomMenu", {room: roomName})
+})
+
+app.get("/chatroom", (req, res) => {
+    res.render("pages/chatroom.ejs")
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
